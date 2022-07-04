7816e095a2b10829cf2f9fca1654851964cd8d9f
Jagger De Leo <jagger@suborbital.dev>
2022-07-04 14:58:13 -0400

    Catch up with upstream
    Uses `gofmt` now

--- crates/gen-tinygo/src/lib.rs
+++ crates/gen-tinygo/src/lib.rs
@@ -1,7 +1,9 @@
 use heck::*;
+use std::collections::HashMap;
+use std::io::{Read, Write};
 use std::mem;
-use std::collections::{HashMap};
-use wit_bindgen_gen_core::wit_parser::abi::{WasmType};
+use std::process::{Command, Stdio};
+use wit_bindgen_gen_core::wit_parser::abi::WasmType;
 use wit_bindgen_gen_core::{wit_parser::*, Direction, Files, Generator, Ns};
 
 #[derive(Default)]
@@ -27,7 +29,7 @@ pub struct TinyGo {
 
     // Type definitions for the given `TypeId`. This is printed topologically
     // at the end.
-    _types: HashMap<TypeId, wit_bindgen_gen_core::Source>,
+    types: HashMap<TypeId, wit_bindgen_gen_core::Source>,
 }
 
 // struct Func {
@@ -37,7 +39,9 @@ pub struct TinyGo {
 #[derive(Default, Debug, Clone)]
 #[cfg_attr(feature = "structopt", derive(structopt::StructOpt))]
 pub struct Opts {
-    // ...
+    /// Whether or not `gofmt` is executed to format generated code.
+    #[cfg_attr(feature = "structopt", structopt(long))]
+    pub gofmt: bool,
 }
 
 impl Opts {
@@ -69,66 +73,235 @@ impl TinyGo {
         let name = iface.name.to_snake_case();
         self.src.go(&format!("package {}\n\n", name));
     }
+
+    fn print_ty(&mut self, iface: &Interface, ty: &Type) {
+        match ty {
+            Type::Id(id) => {
+                let ty = &iface.types[*id];
+                match &ty.name {
+                    Some(name) => {
+                        self.src.go(&name.to_camel_case());
+                    }
+                    None => match &ty.kind {
+                        TypeDefKind::Type(t) => self.print_ty(iface, t),
+                        TypeDefKind::Enum(_)
+                        | TypeDefKind::Expected(_)
+                        | TypeDefKind::Future(_)
+                        | TypeDefKind::Option(_)
+                        | TypeDefKind::Record(_)
+                        | TypeDefKind::Stream(_)
+                        | TypeDefKind::Tuple(_)
+                        | TypeDefKind::Union(_)
+                        | TypeDefKind::Variant(_) => {
+                            unimplemented!();
+                        }
+                        TypeDefKind::Flags(_) => {
+                            self.src.go("Type");
+                        }
+                        TypeDefKind::List(t) => {
+                            self.src.go("[]");
+                            self.print_ty(iface, t);
+                        }
+                    }
+                }
+            }
+            Type::Handle(id) => {
+                self.src.go(&iface.resources[*id].name.to_camel_case());
+            }
+            Type::Unit => self.src.go("struct{}"),
+            Type::Bool => self.src.go("bool"),
+            Type::Char => self.src.go("rune"),
+            Type::U8 => self.src.go("byte"),
+            Type::S8 => self.src.go("int8"),
+            Type::U16 => self.src.go("uint16"),
+            Type::S16 => self.src.go("int16"),
+            Type::U32 => self.src.go("uint32"),
+            Type::S32 => self.src.go("int32"),
+            Type::U64 => self.src.go("uint64"),
+            Type::S64 => self.src.go("int64"),
+            Type::Float32 => self.src.go("float32"),
+            Type::Float64 => self.src.go("float64"),
+            Type::String => self.src.go("string"),
+        }
+    }
+
+    fn print_anonymous_type(&mut self, iface: &Interface, ty: TypeId) {
+        // let prev = mem::take(&mut self.src.go);
+
+        let kind = &iface.types[ty].kind;
+        match kind {
+            TypeDefKind::Type(_)
+            | TypeDefKind::Flags(_)
+            | TypeDefKind::Record(_)
+            | TypeDefKind::Enum(_)
+            | TypeDefKind::Variant(_)
+            | TypeDefKind::Union(_) => {
+                unreachable!()
+            }
+            _ => {
+                unimplemented!("anonymous types")
+            }
+        }
+        // self.types
+        //     .insert(ty, mem::replace(&mut self.src.go, prev));
+    }
+
+    fn is_empty_type(&self, iface: &Interface, ty: &Type) -> bool {
+        let id = match ty {
+            Type::Id(id) => *id,
+            _ => return false,
+        };
+        match &iface.types[id].kind {
+            TypeDefKind::Type(t) => self.is_empty_type(iface, t),
+            TypeDefKind::Record(r) => r.fields.is_empty(),
+            _ => false,
+        }
+    }
 }
 
 #[derive(Default)]
 struct Source {
-    src: wit_bindgen_gen_core::Source,
-    binding_header: wit_bindgen_gen_core::Source,
+    go: wit_bindgen_gen_core::Source,
+    // binding_header: wit_bindgen_gen_core::Source,
+}
+
+impl Source {
+    fn go(&mut self, s: &str) {
+        self.go.push_str(s);
+    }
+
+    // fn binding_header(&mut self, s: &str) {
+    //     self.binding_header.push_str(s);
+    // }
 }
 
 impl Generator for TinyGo {
     fn preprocess_one(&mut self, iface: &Interface, dir: Direction) {
         self.print_package(iface);
-        
         drop(dir);
+    }
+
+    fn type_alias(
+        &mut self,
+        _iface: &Interface,
+        _id: TypeId,
+        _name: &str,
+        _ty: &Type,
+        docs: &Docs,
+    ) {
+        self.docs(docs);
+
+        unimplemented!("type_alias")
     }
 
     fn type_record(
         &mut self,
         iface: &Interface,
-        _id: TypeId,
+        id: TypeId,
         name: &str,
         record: &Record,
         docs: &Docs,
     ) {
-        // let prev = mem::take(&mut self.src);
+        let prev = mem::take(&mut self.src.go);
         self.docs(docs);
-        if record.is_flags() {
-            let name = name.to_camel_case();
-            let type_name = format!("{}Type", name);
-            self.names.insert(&type_name).unwrap();
-            
-            let repr = iface
-                .flags_repr(record)
-                .expect("unsupported number of flags");
+        let name = name.to_camel_case();
 
-            self.src.go(&format!(
-                "type {} {}\n\n",
-                type_name,
-                int_repr(repr),
-            ));
+        self.names.insert(&name).unwrap();
 
-            self.src.go("const (\n");
-            for (i, field) in record.fields.iter().enumerate() {
-                let field_name = format!("{}{}", name, field.name.to_camel_case());
-                self.names.insert(&field_name).unwrap();
-
-                if i == 0 {
-                    self.src.go(&format!(
-                        "\t{} {} = 1 << iota\n",
-                        field_name,
-                        type_name
-                    ));
-                } else {
-                    self.src.go(&format!("\t{}\n", field_name));
-                }
-            }
-            self.src.go(")\n\n"); // END: const (
+        self.src.go(&format!("type {} struct {{\n", name,));
+        for field in record.fields.iter() {
+            self.src.go(&format!("\t{}", field.name.to_camel_case()));
+            self.src.go(" ");
+            self.print_ty(iface, &field.ty);
+            self.src.go("\n");
         }
+        self.src.go("}\n\n");
 
-        // self.types
-        //     .insert(id, mem::replace(&mut self.src.src, prev.src));
+        self.types.insert(id, mem::replace(&mut self.src.go, prev));
+    }
+
+    fn type_enum(&mut self, _iface: &Interface, _id: TypeId, _name: &str, _enum_: &Enum, _docs: &Docs) {
+        unimplemented!("type_enum")
+    }
+
+    fn type_expected(
+        &mut self,
+        _iface: &Interface,
+        _id: TypeId,
+        _name: &str,
+        _expected: &Expected,
+        _docs: &Docs,
+    ) {
+        unimplemented!("type_expected")
+    }
+
+    fn type_flags(
+        &mut self,
+        _iface: &Interface,
+        id: TypeId,
+        name: &str,
+        flags: &Flags,
+        docs: &Docs,
+    ) {
+        let prev = mem::take(&mut self.src.go);
+        let type_name = format!("{}Type", name);
+        self.docs(docs);
+
+        self.names.insert(&type_name).unwrap();
+
+        let repr = flags_repr(flags);
+
+        self.src
+            .go(&format!("type {} {}\n\n", type_name, int_repr(repr),));
+
+        self.src.go("const (\n");
+        for (i, flag) in flags.flags.iter().enumerate() {
+            let field_name = format!("{}{}", name, flag.name.to_camel_case());
+            self.names.insert(&field_name).unwrap();
+
+            if i == 0 {
+                self.src
+                    .go(&format!("{} {} = 1 << iota\n", field_name, type_name));
+            } else {
+                self.src.go(&format!("{}\n", field_name));
+            }
+        }
+        self.src.go(")\n\n");
+
+        self.types.insert(id, mem::replace(&mut self.src.go, prev));
+    }
+
+    fn type_union(
+        &mut self,
+        _iface: &Interface,
+        _id: TypeId,
+        _name: &str,
+        _union: &Union,
+        _docs: &Docs,
+    ) {
+        unimplemented!("type_option")
+    }
+
+    fn type_option(
+        &mut self,
+        _iface: &Interface,
+        _id: TypeId,
+        _name: &str,
+        _payload: &Type,
+        _docs: &Docs,
+    ) {
+        unimplemented!("type_option")
+    }
+
+    fn type_tuple(
+        &mut self,
+        _iface: &Interface,
+        _id: TypeId,
+        _name: &str,
+        _flags: &Tuple,
+        _docs: &Docs,
+    ) {
+        unimplemented!("type_tuple")
     }
 
     fn type_variant(
@@ -141,61 +314,31 @@ impl Generator for TinyGo {
     ) {
         self.docs(docs);
 
-        unimplemented!("type_variant unimplemented")
+        unimplemented!("type_variant")
     }
 
     fn type_resource(&mut self, iface: &Interface, ty: ResourceId) {
         drop((iface, ty));
     }
 
-    fn type_alias(&mut self, _iface: &Interface, _id: TypeId, _name: &str, _ty: &Type, docs: &Docs) {
+    fn type_list(&mut self, iface: &Interface, _id: TypeId, name: &str, ty: &Type, docs: &Docs) {
         self.docs(docs);
 
-        unimplemented!("type_alias unimplemented")
-    }
-
-    fn type_list(&mut self, _iface: &Interface, _id: TypeId, _name: &str, _ty: &Type, docs: &Docs) {
-        self.docs(docs);
-
-        unimplemented!("type_list unimplemented")
-    }
-
-    fn type_pointer(
-        &mut self,
-        iface: &Interface,
-        _id: TypeId,
-        name: &str,
-        const_: bool,
-        ty: &Type,
-        docs: &Docs,
-    ) {
-        drop((iface, _id, name, const_, ty, docs));
+        if *ty == Type::Char {
+            self.src.go("type ");
+            self.src.go(&name.to_camel_case());
+            self.src.go("string");
+        } else {
+            self.src.go("type ");
+            self.src.go(&name.to_camel_case());
+            self.src.go(" []");
+            self.print_ty(iface, ty);
+        }
+        self.src.go("\n");
     }
 
     fn type_builtin(&mut self, iface: &Interface, _id: TypeId, name: &str, ty: &Type, docs: &Docs) {
         drop((iface, _id, name, ty, docs));
-    }
-
-    fn type_push_buffer(
-        &mut self,
-        iface: &Interface,
-        id: TypeId,
-        name: &str,
-        ty: &Type,
-        docs: &Docs,
-    ) {
-        self.type_pull_buffer(iface, id, name, ty, docs);
-    }
-
-    fn type_pull_buffer(
-        &mut self,
-        _iface: &Interface,
-        _id: TypeId,
-        _name: &str,
-        _ty: &Type,
-        _docs: &Docs,
-    ) {
-        unimplemented!("type_pull buffer unimplemented")
     }
 
     fn import(&mut self, iface: &Interface, func: &Function) {
@@ -211,31 +354,51 @@ impl Generator for TinyGo {
     }
 
     fn finish_one(&mut self, iface: &Interface, files: &mut Files) {
-        let src = mem::take(&mut self.src);
+        let mut src = mem::take(&mut self.src.go);
+
         let name = iface.name.to_snake_case();
 
-        files.push(&format!("{}/{}.go", name, name), src.src.as_bytes());
+        for id in iface.topological_types() {
+            if let Some(ty) = self.types.get(&id) {
+                src.push_str(ty);
+            }
+        }
+
+        if self.opts.gofmt {
+            let mut child = Command::new("gofmt")
+                .stdin(Stdio::piped())
+                .stdout(Stdio::piped())
+                .spawn()
+                .expect("failed to spawn `gofmt`");
+            child
+                .stdin
+                .take()
+                .unwrap()
+                .write_all(src.as_bytes())
+                .unwrap();
+            src.as_mut_string().truncate(0);
+            child
+                .stdout
+                .take()
+                .unwrap()
+                .read_to_string(src.as_mut_string())
+                .unwrap();
+            let status = child.wait().unwrap();
+            assert!(status.success());
+        }
+
+        files.push(&format!("{}/{}.go", name, name), src.as_bytes());
     }
 }
 
-impl Source {
-    fn go(&mut self, s: &str) {
-        self.src.push_str(s);
-    }
-
-    fn binding_header(&mut self, s: &str) {
-        self.binding_header.push_str(s);
-    }
-}
-
-fn wasm_type(ty: WasmType) -> &'static str {
-    match ty {
-        WasmType::I32 => "int",
-        WasmType::I64 => "int64",
-        WasmType::F32 => "float32",
-        WasmType::F64 => "float64",
-    }
-}
+// fn wasm_type(ty: WasmType) -> &'static str {
+//     match ty {
+//         WasmType::I32 => "int",
+//         WasmType::I64 => "int64",
+//         WasmType::F32 => "float32",
+//         WasmType::F64 => "float64",
+//     }
+// }
 
 fn int_repr(ty: Int) -> &'static str {
     match ty {
@@ -243,5 +406,15 @@ fn int_repr(ty: Int) -> &'static str {
         Int::U16 => "uint16",
         Int::U32 => "uint32",
         Int::U64 => "uint64",
+    }
+}
+
+fn flags_repr(f: &Flags) -> Int {
+    match f.repr() {
+        FlagsRepr::U8 => Int::U8,
+        FlagsRepr::U16 => Int::U16,
+        FlagsRepr::U32(1) => Int::U32,
+        FlagsRepr::U32(2) => Int::U64,
+        repr => panic!("unimplemented flags {:?}", repr),
     }
 }
